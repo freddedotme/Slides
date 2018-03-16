32748abb554b65fc910e98495a3bea3ea0673048
freddedotme <d6gg0vxb7w27r1ks@icloud.com>
2018-03-16 21:18:26 +0100

    fixed vendor

--- assets/slides.js
+++ assets/slides.js
@@ -19,8 +19,11 @@ if (SIZE > 0) {
   }
 
   function translate3D(el, x, y, z) {
-    el.style.webkitTransform = "translate3D(" + x + ", " + y + ", " + z + ")";
-    el.style.transform = "translate3D(" + x + ", " + y + ", " + z + ")";
+    el.style['-webkit-transform'] = "translate3D(" + x + ", " + y + ", " + z + ")";
+    el.style['-moz-transform'] = "translate3D(" + x + ", " + y + ", " + z + ")";
+    el.style['-ms-transform'] = "translate3D(" + x + ", " + y + ", " + z + ")";
+    el.style['-o-transform'] = "translate3D(" + x + ", " + y + ", " + z + ")";
+    el.style['transform'] = "translate3D(" + x + ", " + y + ", " + z + ")";
   }
 
   animate(SLIDES[0]);
